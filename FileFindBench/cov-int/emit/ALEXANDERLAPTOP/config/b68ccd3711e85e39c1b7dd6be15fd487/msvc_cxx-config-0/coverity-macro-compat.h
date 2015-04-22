c9aa0843160f945b9a0a515949633e5a9d0cb6c8
Alexander Riccio <alexander@riccio.com>
2015-04-21 23:34:35 -0400

    First Coverity build - and some other crap?

---
+++ FileFindBench/cov-int/emit/ALEXANDERLAPTOP/config/b68ccd3711e85e39c1b7dd6be15fd487/msvc_cxx-config-0/coverity-macro-compat.h
@@ -0,0 +1,40 @@
+﻿
+#if !(defined(_MSC_VER) || defined(__coverity_undefine__MSC_VER))
+#define _MSC_VER 1800
+#endif
+#if !(defined(_MSC_FULL_VER) || defined(__coverity_undefine__MSC_FULL_VER))
+#define _MSC_FULL_VER 180031101
+#endif
+#if !(defined(_M_IX86) || defined(__coverity_undefine__M_IX86))
+#define _M_IX86 600
+#endif
+#if !(defined(_WIN32) || defined(__coverity_undefine__WIN32))
+#define _WIN32 1
+#endif
+#if !(defined(_M_IX86_FP) || defined(__coverity_undefine__M_IX86_FP))
+#define _M_IX86_FP 2
+#endif
+#if !(defined(__COUNTER__) || defined(__coverity_undefine___COUNTER__))
+#define __COUNTER__ 0
+#endif
+#if !(defined(__cplusplus) || defined(__coverity_undefine___cplusplus))
+#define __cplusplus 199711L
+#endif
+#if !(defined(_CPPRTTI) || defined(__coverity_undefine__CPPRTTI))
+#define _CPPRTTI 1
+#endif
+#if !(defined(_INTEGRAL_MAX_BITS) || defined(__coverity_undefine__INTEGRAL_MAX_BITS))
+#define _INTEGRAL_MAX_BITS 64
+#endif
+#if !(defined(_MSC_BUILD) || defined(__coverity_undefine__MSC_BUILD))
+#define _MSC_BUILD 0
+#endif
+#if !(defined(_MSC_EXTENSIONS) || defined(__coverity_undefine__MSC_EXTENSIONS))
+#define _MSC_EXTENSIONS 1
+#endif
+#if !(defined(_MT) || defined(__coverity_undefine__MT))
+#define _MT 1
+#endif
+#if !(defined(_NATIVE_NULLPTR_SUPPORTED) || defined(__coverity_undefine__NATIVE_NULLPTR_SUPPORTED))
+#define _NATIVE_NULLPTR_SUPPORTED 1
+#endif
